0495925aba5b89ee61641549ca699db27b3d1d19
FermiNA29 <58778153+FermiNA29@users.noreply.github.com>
2020-10-18 18:50:56 +0700

    Tugas PBO pecahan

---
+++ src/pboif2/pkg10119043/latihan8/pecahan/PBOIF210119043Latihan8Pecahan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119043.latihan8.pecahan;
+
+/**
+ *
+ * @author 
+ * NAMA : FERMI NAUFAL AKBAR
+ * KELAS     : IF2 
+ * NIM       : 10119043 
+ * DESKRIPSI : Program ini berisi program dengan tipe data pecahan
+ */
+public class PBOIF210119043Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("float : " + f);
+        System.out.println("double : " + d);
+    }
+    
+}
